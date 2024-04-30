eaee1f91fe6e98dd9beca225bbb3efeb1b3e2be3
yhwit30 <yhwit30@gmail.com>
2024-04-30 17:05:33 +0900

    url 변경

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Controller
 public class UsrHomeController {
 
-	@RequestMapping("/usr/home/getArticle")
+	@RequestMapping("https://port-0-test-1pgyr2mlvm3ky9e.sel5.cloudtype.app/usr/home/getArticle")
 	@ResponseBody
 	public Article getArticle() {
 		Article article = new Article(1, "제목1");
@@ -24,7 +24,7 @@ public class UsrHomeController {
 		return article;
 	}
 
-	@RequestMapping("/usr/home/getList")
+	@RequestMapping("https://port-0-test-1pgyr2mlvm3ky9e.sel5.cloudtype.app/usr/home/getList")
 	@ResponseBody
 	public List<String> getList() {
 		List<String> list = new ArrayList<>();
